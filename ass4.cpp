6688b8b807dd48079aa46e88079c19dbbafa89e8
Sonia Thakur <soniathakursonia068@gmail.com>
2025-08-15 10:19:50 -0700

    Shorten code by removing extra lines

--- ass4.cpp
+++ ass4.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
 using namespace std;
-
-
 class node {
 public:
     string data;
     node* next;
-
     node(){
         data=" ";
         next=0;
     }
-
     node(string song){
         data = song;
         next = NULL;
     }
 };
-
-
 class playlist {
 public:
     node* head;
@@ -26,7 +20,6 @@ public:
     playlist() {
         head = NULL;  
     }
-
     void insertend(string song);
     void insertbeg(string song);
     void delbeg();
@@ -35,8 +28,6 @@ public:
     void navigate(string key);
     void print();
 };
-
-
     void playlist:: insertend(string song) {
         node* newnode = new node(song);
          if(head == NULL) {
@@ -49,8 +40,6 @@ public:
         }
         t->next = newnode;  
     }
-
-
     void playlist::insertbeg(string song)
     {
         node* newnode = new node(song);
@@ -61,10 +50,7 @@ public:
         else{
         newnode-> next =head;
         head=newnode;
-    }
-}
-
-    
+    }}
     void playlist::delbeg(){
         if (head==NULL){
             cout<<"Playlist is empty";
@@ -74,10 +60,7 @@ public:
             node* temp = head;
             head = head->next;
             delete temp;
-    }
-    }
-
-    
+    }}
     void playlist::delend(){
         if(head==NULL){
             cout<<"Playlist is empty";
@@ -90,10 +73,7 @@ public:
             }
             delete t->next;
              t->next = NULL;
-        }
-        }
-    
-      
+        }}
     void playlist::delpos(int pos) {
     if (head == NULL) {
         cout << "LL is empty" << endl;
@@ -128,12 +108,9 @@ public:
         cout << "Index out of bounds." << endl;
         return;
     }
-
     prev->next = temp->next;
     delete temp;
 }
-
-
 
 void playlist::navigate(string key) {
     if (head == NULL) {
@@ -149,27 +126,19 @@ void playlist::navigate(string key) {
         prev = temp;
         temp = temp->next;
     }
-
     if (temp == NULL) {
         cout << "Song not found" << endl;
         return;
     }
-
-
     if (prev != NULL)
         cout <<"Previous Song:"<<prev->data << endl;
     else
         cout << "Previous song is not found (Key is at head)" << endl;
-
-
     if (temp->next != NULL)
         cout <<"NEXT SONG :"<< temp->next->data << endl;
     else
         cout << "Next song is not found (Key is at tail)" << endl;
 }
-
-
-
  void playlist::print() {
         if (head == NULL) {
             cout << "Playlist is empty"<<endl;
@@ -182,9 +151,7 @@ void playlist::navigate(string key) {
         }
             cout<<"NULL";
             cout<<endl;
-      
     }
-
 
 int main(){
       playlist obj;
@@ -206,63 +173,47 @@ int main(){
 
         cout<<"Enter your choice:\n";
         cin>>choice;
-     
-
         switch(choice) {
             case 1:
                 cout << "Enter song to insert at beginning: ";
                 cin.ignore();
                 getline(cin,song);
                 obj.insertbeg(song);
-                
-                break;
 
+                break;
             case 2:
                 cout << "Enter song to insert at end: ";
                 cin.ignore();
                 getline(cin,song);
                 obj.insertend(song);
                 break;
-
             case 3:
                 obj.delend();
                 break;
-
             case 4:
                 obj.delbeg();
                 break;
-
             case 5:
                 cout<<"Enter the song's position you want to delete:\n";
                 cin>>pos;
                 obj.delpos( pos);
                 break;
-
-
             case 6:
                 cout<<"Enter the song or key which is  used to navigate previous and next song :\n";
                 cin.ignore();
                 getline(cin,key);
                 obj.navigate( key);
                 break;
-
             case 7:
                 obj.print();
                 break;
-                
             case 8:
                 cout<<"EXIT"<<endl;
                 break;
-
             default:
                 cout << "Invalid choice\n";
                 break;
-        }
-
-    } while(choice != 8);
-
-
-
+        } } while(choice != 8);
 
 return 0;
 }
@@ -272,3 +223,4 @@ return 0;
 
 
    
+
